4d99616e9570da5765b36929eb2624103870bb0b
Rai <megac154@gmail.com>
2024-04-10 09:16:38 -0300

    feat: create controller for user

---
+++ api/controllers/userController.js
@@ -0,0 +1,17 @@
+const UserService = require('../services/userService');
+
+const userService = new UserService();
+
+class UserController {
+  async createUser(req, res) {
+    try {
+      const { name, email, password } = req.body;
+      const user = await userService.create({ name, email, password });
+      res.status(201).send(user);
+    } catch (error) {
+      res.status(400).send({ message: error.message });      
+    }
+  }
+}
+
+module.exports = UserController;
