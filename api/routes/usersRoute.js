09f9c3f14c64d2d7eb2ec1201c5bc90b34dc9904
Rai <megac154@gmail.com>
2024-04-15 20:47:10 -0300

    feat: implement middleware of the authenticate in the project

--- api/routes/usersRoute.js
+++ api/routes/usersRoute.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const UserController = require('../controllers/userController');
+const authenticate = require('../middleware/authenticate');
 
 const router = Router();
+
+router.use(authenticate);
 
 router
   .post('/users', UserController.createUser)
