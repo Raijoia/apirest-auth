699db2177d6613a614c1a613761ddb2eced71e9e
Rai <megac154@gmail.com>
2024-04-22 20:39:01 -0300

    feat: add middleware for acess

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -1,13 +1,14 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
+const roles = require('../middleware/roles')
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', ProdutoController.buscarTodosProdutos)
+  .get('/produto', roles(["Gerente"]),ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
-  .put('/produto/id/:id', ProdutoController.editarProduto)
+  .put('/produto/id/:id', ProdutoController.editarProduto);
 
 module.exports = router
